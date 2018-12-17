2169c52899d93a44a6abb1611e542746bfa9c932
first-name second-nameIngenzi <email@example.comingenziass@gmail.com>
2018-12-17 18:40:41 +0200

    correct condition

--- text.js
+++ text.js
@@ -3,19 +3,19 @@ function myTriangle(){
     var numbery=parseInt(document.getElementById("numbery").value);
     var numberz=parseInt(document.getElementById("numberz").value);
 
-    if(numberx+numbery>numberz && numberx+numberz> numbery && numbery+ numberz> numberx ){
-
-if(numberx === numbery && numberx === numberz){
+    if(numberx+numbery>numberz && numberx+numberz> numbery && numbery+ numberz> numberx )
+    {
+   if(numberx === numbery && numberx === numberz){
     console.log(alert("equilateral triangle"));
-}
-else if(numberx === numbery|| numberx=== numberz || numbery===numberz){
+   }
+   else if(numberx === numbery|| numberx=== numberz || numbery===numberz){
     console.log(alert("isoceles triangle"));
-}
-else if(numberx +numbery > numberz ||numberx + numberz > numbery ||numbery + numberz > numberx){
+  }
+   else if(numberx +numbery > numberz && numberx + numberz > numbery && numbery + numberz > numberx){
     console.log(alert("scalene triangle"));
-}
-else {
+  }
+  else {
     console.log(alert("not a triangle"));
-}
-}
+    
+    }
 }
